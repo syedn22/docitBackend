eb8164c7d369156503b295c035dc83950e4d3796
syed mohammed <syedmohammedn22@gmail.com>
2021-04-15 11:09:39 +0530

    Get By Id endpoint added

--- Routes/User.js
+++ Routes/User.js
@@ -1,14 +1,20 @@
-const express = require('express');
+const express = require("express");
 const Router = express.Router();
-const {getUser,UpdateUser,deleteUser,deleteUsers,InsertUser} =require('../Controller/UserController');
+const {
+  getUser,
+  getUsers,
+  UpdateUser,
+  deleteUser,
+  deleteUsers,
+  InsertUser,
+} = require("../Controller/UserController");
 
-
-Router.get('/',getUser)
- Router.post('/', InsertUser)
+Router.get("/", getUsers);
+Router.get("/:id", getUser);
+Router.post("/", InsertUser);
 //Router.post('/', InsertUsers)
-Router.put('/:id',UpdateUser)
-Router.delete('/:id',deleteUser)
-Router.delete('/',deleteUsers);
-
+Router.put("/:id", UpdateUser);
+Router.delete("/:id", deleteUser);
+Router.delete("/", deleteUsers);
 
 module.exports = Router;
--- index.js
+++ index.js
@@ -1,35 +1,46 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 dotenv.config();
-const auth = require('./Authentication/auth');
-const classroom=require('./Routes/Classroom');
-const user=require('./Routes/User')
+const auth = require("./Authentication/auth");
+const classroom = require("./Routes/Classroom");
+const user = require("./Routes/User");
 
 app.use(express.json());
 
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+
+app.use(allowCrossDomain);
+
 const connectionstring = `mongodb+srv://Hariharan:${process.env.MONGODB_PASSWORD}@cluster0.kxkqd.mongodb.net/CollegeProject?retryWrites=true&w=majority`;
 
-mongoose.connect(connectionstring, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-    .then(() => {
-        console.log("Connected Successfully To mongodb")
-    })
-    .catch(e => {
-        console.log(e);
-    })
+mongoose
+  .connect(connectionstring, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Connected Successfully To mongodb");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
-app.use('/auth', auth);
-app.use('/Classroom',classroom);
-app.use('/User',user);
+app.use("/auth", auth);
+app.use("/Classroom", classroom);
+app.use("/User", user);
 
-app.get('/', (req, res) => {
-    res.send("Home")
-})
-
+app.get("/", (req, res) => {
+  res.send("Home");
+});
 
 app.listen(process.env.PORT, () => {
-    console.log("Port is created in", process.env.PORT);
-})
-
-
+  console.log("Port is created in", process.env.PORT);
+});
